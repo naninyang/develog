bc6a1eca896bc96217f888ae405614a893946bec
Chloe <gtalk.with.nani@gmail.com>
2023-12-15 23:52:04 +0900

    .

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,11 +97,5 @@ module.exports = {
         mergeSecurityHeaders: false, // boolean to turn off the default security headers
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: `G-TMB7WVNG1X`,
-      },
-    },
   ],
 };
